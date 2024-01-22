19c554f76c9dac5929ba8b62d5f78217270b335a
choijh <choijh9023@naver.com>
2024-01-22 18:29:32 +0900

    정처기

--- java/java_inflearn/Ex1.java
+++ java/java_inflearn/Ex1.java
@@ -1,7 +1,17 @@
 package java_inflearn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //24년 1월
 public class Ex1 {
     public static void main(String[] args) {
-
+        List<Integer> answer = new ArrayList<>();
+        answer.add(0,4);
+        answer.add(1,3);
+        System.out.println(answer.get(1));
+        String a = "aa";
+        System.out.println(a+a);
     }
 }
